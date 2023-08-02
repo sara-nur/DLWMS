508179f4e4b21ca282bdb5d42848cb096ed351ba
Sara Nuredinovski <saranur.cs@gmail.com>
2023-08-02 19:32:27 +0200

    DLWMS initial commit

---
+++ DLWMS/DLWMS.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+
+namespace DLWMS.Console
+{
+
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            Console.WriteLine ("Hello World");
+        }
+    }
+}
---
+++ DLWMS/DLWMS.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace DLWMS.ConsoleApp
+{
+
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            Console.WriteLine ("Hello World");
+             
+        }
+    }
+}
